0cd9e27fb4f8110cad0ee088aba2168ce7cc508b
Eranda Upeshitha <upeshitha@gmail.com>
2021-06-30 18:50:16 +0530

    The initial project

---
+++ Employee Leave Managent/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Employee_Leave_Managent.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

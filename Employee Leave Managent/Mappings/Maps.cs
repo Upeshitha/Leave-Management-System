4e7d536307f0c4c77789ea5d8913b319f3f88c27
Eranda Upeshitha <upeshitha@gmail.com>
2021-07-01 08:22:57 +0530

    Create View Model Classes
    add auto mapper dependency and  map with view model classes and data classes.

---
+++ Employee Leave Managent/Mappings/Maps.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Employee_Leave_Managent.Data;
+using Employee_Leave_Managent.Data.Migrations;
+using Employee_Leave_Managent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Leave_Managent.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+        }
+    }
+}
--- Employee Leave Managent/Models/LeaveTypeVM.cs
+++ Employee Leave Managent/Models/LeaveTypeVM.cs
@@ -4,13 +4,18 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Employee_Leave_Managent.Data
+namespace Employee_Leave_Managent.Models
 {
-    public class LeaveType
+    public class DetailsLeaveTypeVM
     {
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateLeaveTypeVM
+    {
+        [Required]
+        public string Name { get; set; }
     }
 }

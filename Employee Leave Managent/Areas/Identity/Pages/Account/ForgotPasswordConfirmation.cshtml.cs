6e30a852d31d0aee9e5495d3ea45d924b86c8037
Eranda Upeshitha <upeshitha@gmail.com>
2021-07-02 06:19:31 +0530

    Disable email confirmed option
    Now did not ask again to confirm email. When user registered, at same time user login to the page.

---
+++ Employee Leave Managent/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Employee_Leave_Managent.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Employee Leave Managent/Startup.cs
+++ Employee Leave Managent/Startup.cs
@@ -42,7 +42,7 @@ namespace Employee_Leave_Managent
 
             services.AddAutoMapper(typeof(Maps));
             
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
